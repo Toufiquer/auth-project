7def8533187dbf50224c8c431604f9507ae6f1e7
Toufiquer <toufiquerabc@gmail.com>
2024-10-17 02:44:30 +0600

    init project and create page, Home, Private Login

---
+++ src/app/login/page.tsx
@@ -0,0 +1,19 @@
+/*
+|-----------------------------------------
+| setting up Page for the App
+| @author: Toufiquer Rahman<toufiquer.0@gmail.com>
+| @copyright: auth-project, October, 2024
+|-----------------------------------------
+*/
+const Page = () => {
+    return (
+        <main>
+            <div className="flex items-center justify-center w-full h-screen">
+                <button className="border border-slate-100 py-2 rounded-full px-6 cursor-pointer hover:bg-slate-500 text-white duration-200">
+                    Google LogIn
+                </button>
+            </div>
+        </main>
+    )
+}
+export default Page
---
+++ src/app/private/page.tsx
@@ -0,0 +1,17 @@
+/*
+|-----------------------------------------
+| setting up Page for the App
+| @author: Toufiquer Rahman<toufiquer.0@gmail.com>
+| @copyright: auth-project, October, 2024
+|-----------------------------------------
+*/
+const Page = () => {
+    return (
+        <main>
+            <div className="flex items-center justify-center w-full h-screen">
+                <h2>Private</h2>
+            </div>
+        </main>
+    )
+}
+export default Page
