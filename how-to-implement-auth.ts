bc68e56b8024256b85f519c8cd87de1859394d2d
Toufiquer <toufiquerabc@gmail.com>
2024-10-23 00:26:35 +0600

    done @ fix bug

---
+++ how-to-implement-auth.ts
@@ -0,0 +1,25 @@
+/**
+ * 
+ * doc: https://authjs.dev/getting-started/authentication/oauth
+
+ * 1. yarn add next-auth@beta
+ * 2. npx auth secret
+ * 3. src/auth.js => [flow the doc]
+ * 4. src/api/auth/[...nextAuth]/route.ts => [flow the doc]
+ * 5. src/middleware.js => [flow the doc]
+ * 
+ * 6. Flow the authentication for Google or Github 
+ * 
+ * 
+ * ## 1. Login with Google auth
+ * 2. if not logIn you can't see private page and private components [home page]
+ * 
+ * 3. log out 
+ * 
+ * 
+ * 
+ * 4. update credentials -> https://console.cloud.google.com/apis/credentials/oauthclient/
+ * 
+ * !! in production it throw an error -> https://i.sstatic.net/dKxaS.png
+ * 
+ * */
